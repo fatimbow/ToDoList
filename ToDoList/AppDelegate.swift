d24d0b546b4897c52fb2aa8adf0017a6ffdb34ae
fatimbow <fatmabasaran217@gmail.com>
2023-05-27 13:18:13 +0300

    Create a custom data model

--- ToDoList/AppDelegate.swift
+++ ToDoList/AppDelegate.swift
@@ -13,10 +13,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    
+        print(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).last! as String)
+        
         return true
     }
-
+    
+    
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
@@ -26,9 +29,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+        print("didDiscardSceneSessions")
     }
 
 
--- ToDoList/Controllers/TodolistViewController.swift
+++ ToDoList/Controllers/TodolistViewController.swift
@@ -9,12 +9,33 @@ import UIKit
 
 class TodolistViewController: UITableViewController {
     
-    var itemArray = ["Exercise", "Read", "Buy groceries"]
+    var itemArray = [Item]()
+    
+    let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let newItem = Item()
+        newItem.title = "Boil the eggs"
+        itemArray.append(newItem)
+        
+        let newItem2 = Item()
+        newItem2.title = "Cut the tomatoes"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "Wash the parsley"
+        itemArray.append(newItem3)
+        
+        
+        
+       // if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+       //     itemArray = items
+       // }
+        
     }
+    
     
     //MARK : Tableview Datasource Methods
     
@@ -27,8 +48,14 @@ class TodolistViewController: UITableViewController {
             
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
-
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        // value = condition ? valueIfTrue: valueIfFalse
+        
+        cell.accessoryType = item.done ? .checkmark : .none
+        
         return cell
 
     }
@@ -38,11 +65,9 @@ class TodolistViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //print(itemArray[indexPath.row])
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        tableView.reloadData()
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
@@ -57,7 +82,12 @@ class TodolistViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add Item", style: .default) { action in
           
-            self.itemArray.append(textField.text!)
+            let newItem = Item()
+            newItem.title = textField.text!
+            
+            self.itemArray.append(newItem)
+            
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
             
             self.tableView.reloadData()
         }
